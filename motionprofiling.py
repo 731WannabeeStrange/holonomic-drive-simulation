b6780eb81313aaa07adde20a039af51711e474bf
Nicholas Day <nick@nickendo.com>
2017-04-09 15:37:50 -0400

    Working motion profiling grapher

--- motionprofiling.py
+++ motionprofiling.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
 import math
+import datetime
+date_string = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
 
 def clear():
     subprocess.run(['adb', 'logcat', '-c'], stdout=subprocess.PIPE)
@@ -14,7 +16,7 @@ def process_log(log):
     a = []
     b = []
     lines = log.split('\n')
-    for line in lines:
+    for q, line in enumerate(lines):
         if len(line) != 0:
             if line[0] != '-':
                 data = line.split()
@@ -22,25 +24,46 @@ def process_log(log):
                 data[-1] = data[-1][:-1]
                 for i, datum in enumerate(data):
                     if i == 0:
-                        a.append(float(data[i]))
+                        if 'E' not in data[i]:
+                            a.append(float(data[i]))
+                        else:
+                            a.append(0)
                     elif i == 1:
-                        b.append(float(data[i]))
+                        if 'E' not in data[i]:
+                            b.append(float(data[i]))
+                        else:
+                            b.append(0)
 
     return [a, b]
 
-def derivative(values, time):
+def derivative(values, time, smooth):
     derivs = []
     for i, (value, sec) in enumerate(zip(values, time)):
         if i != 0:
-            deriv = (values[i] - values[i - 1]) / (time[i] - time[i - 1])
+            dt = time[i] - time[i - 1]
+            if dt == 0:
+                if len(derivs) == (i - 1):
+                    deriv = derivs[i - 2]
+                else:
+                    deriv = 0
+            else:
+                sample_size = smooth
+                deriv = (values[i] - values[i - sample_size]) / (time[i] -
+                        time[i - sample_size])
+            if time[i] < 2.5 and time[i] > 2.3:
+                print("time: ")
+                print(values[i - 1])
+                print(values[i])
+                print(deriv)
+
             derivs.append(deriv)
 
     return derivs
 
-distance = 4 # feet
+distance = 2 # feet
 max_acc = 3 # feet/sec^2
 
-diagonal_distance = 2*distance/math.sqrt(2)
+diagonal_distance = math.sqrt(2)*distance/2
 
 T = math.sqrt((2*math.pi*diagonal_distance)/max_acc) # time to destination
 k_one = 2*math.pi/T # max_acc * k_one is max jerk
@@ -56,22 +79,24 @@ def velocity(t):
 def position(t):
     return k_two*(t - k_three*math.sin(k_one*t))
 
-#log = process_log(get_log())
-#plot(log[0], log[1])
-# derived_time = np.array(log[0])
+log = process_log(get_log())
+clear()
 
 # have to use derived time to calculate 
+x = np.array(log[0])
 
-x = np.linspace(0, T, 100)
+# Used for testing
+# x = np.linspace(0, T, 100)
+
 vposition = np.vectorize(position)
 vvelocity = np.vectorize(velocity)
 vacceleration = np.vectorize(acceleration)
 a = vacceleration(x)
 v = vvelocity(x)
 p = vposition(x)
-derived_position = .95 * p
-derived_velocity = derivative(derived_position, x)
-derived_acceleration = derivative(derived_velocity, x)
+derived_position = np.array(log[1])
+derived_velocity = derivative(derived_position, x, 30)
+derived_acceleration = derivative(derived_velocity, x, 30)
 d_p = derived_position
 d_v = np.append(derived_velocity, [0])
 d_a = np.append(derived_acceleration, [0, 0])
@@ -79,9 +104,12 @@ d_a = np.append(derived_acceleration, [0, 0])
 plt.plot(x, v, 'b-')
 plt.plot(x, a, 'r-')
 plt.plot(x, p, 'g-')
-plt.plot(x, d_v, 'o-')
-plt.plot(x, d_a, 'p-')
-plt.plot(x, d_p, 'v-')
+plt.plot(x, d_v, 'b-')
+plt.plot(x, d_a, 'r-')
+plt.plot(x, d_p, 'g-')
 plt.ylabel('feet')
 plt.xlabel('time')
+
+plt.savefig(date_string + ".png")
+
 plt.show()
